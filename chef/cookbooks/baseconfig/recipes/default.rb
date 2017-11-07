74735f6b5f26b962c15f7289cba54ad4fb794726
Ashton <acharbon@sfu.ca>
2017-11-07 12:21:51 -0800

    Updated chef recipe with postgres and go installation.

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -1,4 +1,4 @@
-# Make sure the Apt package lists are up to date, so we're downloading versions that exist.
+# Make sure the apt package lists are up to date.
 cookbook_file "apt-sources.list" do
   path "/etc/apt/sources.list"
 end
@@ -15,3 +15,43 @@ end
 execute 'ntp_restart' do
   command 'service ntp restart'
 end
+
+# Go installation and Go dependencies installation.
+package "golang"
+execute 'get-pq' do
+  environment 'GOPATH' => '/go'
+  command 'go get -u github.com/lib/pq'
+end
+execute 'get-mux' do
+  environment 'GOPATH' => '/go'
+  command 'go get -u github.com/gorilla/mux'
+end
+
+# Postgres setup.
+package "postgresql"
+execute 'postgres-setup' do
+  command 'echo "CREATE DATABASE pokerdb;" | sudo -u postgres psql'
+end
+execute 'postgres-set-password' do
+  command 'echo "ALTER USER postgres WITH PASSWORD \'postgres\';" | sudo -u postgres psql'
+end
+execute 'database-setup' do
+  cwd '/vagrant'
+  command 'sudo -u postgres psql pokerdb -f schema.sql'
+end
+
+# Install tmux and start the server in the background.
+package "tmux"
+execute 'create-server-session' do
+  cwd '/vagrant'
+  environment 'GOPATH' => '/go'
+  command 'tmux new-session -d -s server'
+end
+execute 'start-server' do
+#  command "tmux send-keys -t server 'go run poker470.go' C-m"
+end
+
+# To access the program/prompt from inside the vm:
+# vagrant ssh
+# sudo su
+# tmux attach -t server
