0ae139ca277ab7fed953d1514c796cec02b8a26a
Ashton <ashton@ashtonc.ca>
2017-11-16 22:11:44 -0800

    Added skeleton template processing for most pages.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -19,19 +19,57 @@ func HomeRedirect(env *models.Env) http.Handler {
 
 func Home(env *models.Env) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Home")
+		pagedata := models.PageData{
+			Session: models.Session{
+				LoggedIn: true,
+				Username: "current-user",
+				Name: "Current User",
+				PageHome: true,
+			},
+		}
+
+		// Build our template using the required files (need base, head, navigation, and content)
+		// This should be moved to a caching function: https://elithrar.github.io/article/approximating-html-template-inheritance/
+		t, _ := template.ParseFiles("./templates/base.tmpl", "./templates/head_base.tmpl", "./templates/navigation.tmpl", "./templates/index.tmpl")
+
+		// Execute the template with our page data
+		t.Execute(w, pagedata)
 	})
 }
 
 func Login(env *models.Env) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Login")
+		pagedata := models.PageData{
+			Session: models.Session{
+				LoggedIn: false,
+				PageLogin: true,
+			},
+		}
+
+		// Build our template using the required files (need base, head, navigation, and content)
+		// This should be moved to a caching function: https://elithrar.github.io/article/approximating-html-template-inheritance/
+		t, _ := template.ParseFiles("./templates/base.tmpl", "./templates/head_base.tmpl", "./templates/navigation.tmpl", "./templates/login.tmpl")
+
+		// Execute the template with our page data
+		t.Execute(w, pagedata)
 	})
 }
 
 func Register(env *models.Env) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Register")
+		pagedata := models.PageData{
+			Session: models.Session{
+				LoggedIn: false,
+				PageRegister: true,
+			},
+		}
+
+		// Build our template using the required files (need base, head, navigation, and content)
+		// This should be moved to a caching function: https://elithrar.github.io/article/approximating-html-template-inheritance/
+		t, _ := template.ParseFiles("./templates/base.tmpl", "./templates/head_base.tmpl", "./templates/navigation.tmpl", "./templates/register.tmpl")
+
+		// Execute the template with our page data
+		t.Execute(w, pagedata)
 	})
 }
 
@@ -57,7 +95,7 @@ func User(env *models.Env) http.Handler {
 		}
 
 		// Build our template using the required files (need base, head, navigation, and content)
-		// This should be moved to a caching function
+		// This should be moved to a caching function: https://elithrar.github.io/article/approximating-html-template-inheritance/
 		t, _ := template.ParseFiles("./templates/base.tmpl", "./templates/head_base.tmpl", "./templates/navigation.tmpl", "./templates/user_view.tmpl")
 
 		// Execute the template with our page data
@@ -67,13 +105,50 @@ func User(env *models.Env) http.Handler {
 
 func UserEdit(env *models.Env) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Edit a user")
+		vars := mux.Vars(r)
+		username := vars["username"]
+
+		pagedata := models.PageData{
+			Session: models.Session{
+				LoggedIn: true,
+				Username: "current-user",
+				Name: "Current User",
+				PageUser: true,
+			},
+			UserPage: models.UserPage{
+				MatchesSession: true,
+				Username: username,
+				Name: "User Name",
+				Email: "user@email.ca",
+			},
+		}
+
+		// Build our template using the required files (need base, head, navigation, and content)
+		// This should be moved to a caching function: https://elithrar.github.io/article/approximating-html-template-inheritance/
+		t, _ := template.ParseFiles("./templates/base.tmpl", "./templates/head_base.tmpl", "./templates/navigation.tmpl", "./templates/user_edit.tmpl")
+
+		// Execute the template with our page data
+		t.Execute(w, pagedata)
 	})
 }
 
 func Game(env *models.Env) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Game")
+		pagedata := models.PageData{
+			Session: models.Session{
+				LoggedIn: true,
+				Username: "current-user",
+				Name: "Current User",
+				PageGame: true,
+			},
+		}
+
+		// Build our template using the required files (need base, head, navigation, and content)
+		// This should be moved to a caching function: https://elithrar.github.io/article/approximating-html-template-inheritance/
+		t, _ := template.ParseFiles("./templates/base.tmpl", "./templates/head_base.tmpl", "./templates/navigation.tmpl", "./templates/game_lobby.tmpl")
+
+		// Execute the template with our page data
+		t.Execute(w, pagedata)
 	})
 }
 
