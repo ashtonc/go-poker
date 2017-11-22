1a7bce9abf5b5cdc2bccb869b51e32b6587f8ff1
Clayton Jian <claytonj@sfu.ca>
2017-11-22 08:36:22 -0800

    Implemented fold and raise functions

--- static/js/game.js
+++ static/js/game.js
@@ -1,68 +1,55 @@
 $(document).ready(function(){
 	// initialize first turn (random)
-	$('.position1').addClass('.yourTurn');
-	$('.position1').find('img').css({'outline': '5px solid blue'});
+	$('.position1').find('img').addClass('yourTurn');
 	$('.position1').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
 	$('.startTimer').on('animationend', nextTurn);
 	function nextTurn(){
 		// if it's your turn, draw blue outline and animate it
-		if($('.position1').hasClass('.yourTurn')){
-			$('.position1').removeClass('.yourTurn');
-			$('.position1').find('img').css({'outline': 'none'});
+		if($('.position1').find('img').hasClass('yourTurn')){
+			$('.position1').find('img').removeClass('yourTurn');
 			$('.timer').remove();
 			$('.startTimer').remove();
-			$('.position2').addClass('.yourTurn');
-			$('.position2').find('img').css({'outline': '5px solid blue'});
+			$('.position2').find('img').addClass('yourTurn');
 			$('.position2').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
 			$('.startTimer').on('animationend', nextTurn);
 		}
-		else if($('.position2').hasClass('.yourTurn')){
-			$('.position2').removeClass('.yourTurn');
-			$('.position2').find('img').css({'outline': 'none'});
+		else if($('.position2').find('img').hasClass('yourTurn')){
+			$('.position2').find('img').removeClass('yourTurn');
 			$('.timer').remove();
 			$('.startTimer').remove();
-			$('.position3').addClass('.yourTurn');
-			$('.position3').find('img').css({'outline': '5px solid blue'});
+			$('.position3').find('img').addClass('yourTurn');
 			$('.position3').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
 			$('.startTimer').on('animationend', nextTurn);
 		}
-		else if($('.position3').hasClass('.yourTurn')){
-			$('.position3').removeClass('.yourTurn');
-			$('.position3').find('img').css({'outline': 'none'});
+		else if($('.position3').find('img').hasClass('yourTurn')){
+			$('.position3').find('img').removeClass('yourTurn');
 			$('.timer').remove();
 			$('.startTimer').remove();
-			$('.position4').addClass('.yourTurn');
-			$('.position4').find('img').css({'outline': '5px solid blue'});
+			$('.position4').find('img').addClass('yourTurn');
 			$('.position4').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
 			$('.startTimer').on('animationend', nextTurn);
 		}
-		else if($('.position4').hasClass('.yourTurn')){
-			$('.position4').removeClass('.yourTurn');
-			$('.position4').find('img').css({'outline': 'none'});
+		else if($('.position4').find('img').hasClass('yourTurn')){
+			$('.position4').find('img').removeClass('yourTurn');
 			$('.timer').remove();
 			$('.startTimer').remove();
-			$('.position5').addClass('.yourTurn');
-			$('.position5').find('img').css({'outline': '5px solid blue'});
+			$('.position5').find('img').addClass('yourTurn');
 			$('.position5').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
 			$('.startTimer').on('animationend', nextTurn);
 		}
-		else if($('.position5').hasClass('.yourTurn')){
-			$('.position5').removeClass('.yourTurn');
-			$('.position5').find('img').css({'outline': 'none'});
+		else if($('.position5').find('img').hasClass('yourTurn')){
+			$('.position5').find('img').removeClass('yourTurn');
 			$('.timer').remove();
 			$('.startTimer').remove();
-			$('.position6').addClass('.yourTurn');
-			$('.position6').find('img').css({'outline': '5px solid blue'});
+			$('.position6').find('img').addClass('yourTurn');
 			$('.position6').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
 			$('.startTimer').on('animationend', nextTurn);
 		}
 		else{
-			$('.position6').removeClass('.yourTurn');
-			$('.position6').find('img').css({'outline': 'none'});
+			$('.position6').find('img').removeClass('yourTurn');
 			$('.timer').remove();
 			$('.startTimer').remove();
-			$('.position1').addClass('.yourTurn');
-			$('.position1').find('img').css({'outline': '5px solid blue'});
+			$('.position1').find('img').addClass('yourTurn');
 			$('.position1').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
 			$('.startTimer').on('animationend', nextTurn);
 		}
@@ -77,11 +64,15 @@ $(document).ready(function(){
 	});
 	$('.menu').on('click', '#fold', function(){
 		$('.startTimer').stop(true, true);
-		// $('.game').find('.yourTurn').closest('img').css({'opacity': '0.5'});
+		$('.yourTurn').closest('img').addClass('folded');
 		nextTurn();
 	});
-	$('.menu').on('click', '#raise', function(){
+	$('.menu').on('click', '#raiseButton', function(){
 		$('.startTimer').stop(true, true);
+		var bet = $('#raise').val();
+		var stack = $('.yourTurn').parent().find('.stack').text();
+		var amount = stack - bet;
+		$('.yourTurn').parent().find('.stack').text(amount);
 		nextTurn();
 	});
 	$('form').submit(function(e){
