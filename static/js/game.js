7e4b1c6b2a2c54344ced16273676a0b4be9651bd
Clayton Jian <claytonj@sfu.ca>
2017-11-22 11:35:41 -0800

    Added dealing (need to interface with game logic) and variable table sizes

--- static/js/game.js
+++ static/js/game.js
@@ -1,14 +1,36 @@
 $(document).ready(function(){
 	// initialize first turn (random)
 	var numberOfPlayers = 6;
-	deal(numberOfPlayers);
+	addPlayers();
+	deal();
 	$('.position1').find('img').addClass('yourTurn');
 	$('.position1').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
 	$('.startTimer').on('animationend', nextTurn);
-	function deal(num){
-		for(i = 0; i < num * 5; i++){
-			currentPlayer = '.position' + ((i%num) + 1);
-			var rank = (i%num + 2);
+	function addPlayers(){
+		if (numberOfPlayers == 2){
+			$('.bottomRow').append('<div class=\"position2\"><img src=\"profile.jpg\" title =\"picture\"><div class=\"name\">Player 2</div><div class=\"stack\">1000000</div><ul class=\"table\"></ul></div>');
+		}
+		if (numberOfPlayers == 3 || numberOfPlayers == 4){
+			$('.topRow').append('<div class=\"position2\"><img src=\"profile.jpg\" title =\"picture\"><div class=\"name\">Player 2</div><div class=\"stack\">1000000</div><ul class=\"table\"></ul></div>');
+			if (numberOfPlayers == 4){
+				$('.bottomRow').append('<div class=\"position4\"><img src=\"profile.jpg\" title =\"picture\"><div class=\"name\">Player 4</div><div class=\"stack\">1000000</div><ul class=\"table\"></ul></div>');
+			}
+			$('.bottomRow').append('<div class=\"position3\"><img src=\"profile.jpg\" title =\"picture\"><div class=\"name\">Player 3</div><div class=\"stack\">1000000</div><ul class=\"table\"></ul></div>');
+		}
+		if (numberOfPlayers == 5 || numberOfPlayers == 6){
+			$('.topRow').append('<div class=\"position2\"><img src=\"profile.jpg\" title =\"picture\"><div class=\"name\">Player 2</div><div class=\"stack\">1000000</div><ul class=\"table\"></ul></div>');
+			$('.topRow').append('<div class=\"position3\"><img src=\"profile.jpg\" title =\"picture\"><div class=\"name\">Player 3</div><div class=\"stack\">1000000</div><ul class=\"table\"></ul></div>');
+			if (numberOfPlayers == 6){
+				$('.bottomRow').append('<div class=\"position6\"><img src=\"profile.jpg\" title =\"picture\"><div class=\"name\">Player 6</div><div class=\"stack\">1000000</div><ul class=\"table\"></ul></div>');
+			}
+				$('.bottomRow').append('<div class=\"position5\"><img src=\"profile.jpg\" title =\"picture\"><div class=\"name\">Player 5</div><div class=\"stack\">1000000</div><ul class=\"table\"></ul></div>');
+				$('.bottomRow').append('<div class=\"position4\"><img src=\"profile.jpg\" title =\"picture\"><div class=\"name\">Player 4</div><div class=\"stack\">1000000</div><ul class=\"table\"></ul></div>');
+		}
+	}
+	function deal(){
+		for(i = 0; i < numberOfPlayers * 5; i++){
+			currentPlayer = '.position' + ((i%numberOfPlayers) + 1);
+			var rank = (i%numberOfPlayers + 2);
 			var suit = 'spades';
 			$(currentPlayer).find('.table').append('<li><div class=\"card rank-' + rank + ' ' + suit + '\"><span class=\"rank\">' + rank + '</span><span class=\"suit\">&' + suit + ';</span></div></li>');
 		}
@@ -29,36 +51,64 @@ $(document).ready(function(){
 			$('.position2').find('img').removeClass('yourTurn');
 			$('.timer').remove();
 			$('.startTimer').remove();
-			$('.position3').find('img').addClass('yourTurn');
-			$('.position3').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
-			$('.startTimer').on('animationend', nextTurn);
+			if(numberOfPlayers >= 3){
+				$('.position3').find('img').addClass('yourTurn');
+				$('.position3').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
+				$('.startTimer').on('animationend', nextTurn);
+			}
+			else{
+				$('.position1').find('img').addClass('yourTurn');
+				$('.position1').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
+				$('.startTimer').on('animationend', nextTurn);
+			}
 		}
 		else if($('.position3').find('img').hasClass('yourTurn')){
 			$('.yourTurn').parent().addClass('folded');
 			$('.position3').find('img').removeClass('yourTurn');
 			$('.timer').remove();
 			$('.startTimer').remove();
-			$('.position4').find('img').addClass('yourTurn');
-			$('.position4').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
-			$('.startTimer').on('animationend', nextTurn);
+			if(numberOfPlayers >= 4){
+				$('.position4').find('img').addClass('yourTurn');
+				$('.position4').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
+				$('.startTimer').on('animationend', nextTurn);
+			}
+			else{
+				$('.position1').find('img').addClass('yourTurn');
+				$('.position1').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
+				$('.startTimer').on('animationend', nextTurn);
+			}
 		}
 		else if($('.position4').find('img').hasClass('yourTurn')){
 			$('.yourTurn').parent().addClass('folded');
 			$('.position4').find('img').removeClass('yourTurn');
 			$('.timer').remove();
 			$('.startTimer').remove();
-			$('.position5').find('img').addClass('yourTurn');
-			$('.position5').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
-			$('.startTimer').on('animationend', nextTurn);
+			if(numberOfPlayers >= 5){
+				$('.position5').find('img').addClass('yourTurn');
+				$('.position5').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
+				$('.startTimer').on('animationend', nextTurn);
+			}
+			else{
+				$('.position1').find('img').addClass('yourTurn');
+				$('.position1').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
+				$('.startTimer').on('animationend', nextTurn);
+			}
 		}
 		else if($('.position5').find('img').hasClass('yourTurn')){
 			$('.yourTurn').parent().addClass('folded');
 			$('.position5').find('img').removeClass('yourTurn');
 			$('.timer').remove();
 			$('.startTimer').remove();
-			$('.position6').find('img').addClass('yourTurn');
-			$('.position6').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
-			$('.startTimer').on('animationend', nextTurn);
+			if(numberOfPlayers == 6){
+				$('.position6').find('img').addClass('yourTurn');
+				$('.position6').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
+				$('.startTimer').on('animationend', nextTurn);
+			}
+			else{
+				$('.position1').find('img').addClass('yourTurn');
+				$('.position1').find('img').after('<div class=\"timer\"><div class=\"startTimer\"></div></div>');
+				$('.startTimer').on('animationend', nextTurn);
+			}
 		}
 		else{
 			$('.yourTurn').parent().addClass('folded');
