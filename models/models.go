0459fcbe30320342d15c286ba1f64a36e5b4cb1e
Ashton Charbonneau <acharbon@sfu.ca>
2017-11-25 16:26:14 -0800

    Significantly updated model and templates.

--- models/models.go
+++ models/models.go
@@ -9,17 +9,9 @@ import (
 // http://www.alexedwards.net/blog/organising-database-access for the idea
 type Env struct {
 	Database *sql.DB
-	// template cache
+	// template cache middleware
 	// logger middleware
 	// authentication middleware
-}
-
-type UserPage struct {
-	MatchesSession bool
-	Username string
-	Name string
-	Email string
-	PictureUrl string
 }
 
 type Session struct {
@@ -34,8 +26,55 @@ type Session struct {
 	PageLeaderboard bool
 }
 
-type Game struct {
+type UserPage struct {
+	MatchesSession bool
+	Username string
+	Name string
+	Email string
+	PictureUrl string
+}
+
+type Card struct {
 	
+}
+
+type GameDeck struct {
+	Cards []Card
+}
+
+type GameHand struct {
+	Card1 Card
+	Card2 Card
+	Card3 Card
+	Card4 Card
+	Card5 Card
+}
+
+type GameSeat struct {
+	Cash int64
+	Hand GameHand
+}
+
+type GameStakes struct {
+	Ante int64
+	MaxBet int64
+	MinBet int64
+}
+
+type GamePhase struct {
+	
+}
+
+type Game struct {
+	Stakes GameStakes
+	Phase GamePhase
+	Deck GameDeck
+	Player1 GameSeat
+	Player2 GameSeat
+	Player3 GameSeat
+	Player4 GameSeat
+	Player5 GameSeat
+	Player6 GameSeat
 }
 
 type Lobby struct {
