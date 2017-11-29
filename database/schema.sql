a38c535a573a3c9e77086b423af2ca927ed51fd7
Ashton Charbonneau <acharbon@sfu.ca>
2017-11-28 23:27:38 -0800

    Updated schema.

--- database/schema.sql
+++ database/schema.sql
@@ -3,43 +3,79 @@ CREATE TABLE user (
 	username VARCHAR(32),
 	name VARCHAR(256),
 	email VARCHAR(128),
-	picture INT,
+	picture_slug VARCHAR(128),
 	description TEXT,
-	password_hash VARCHAR(256),
-	password_salt VARCHAR(256)
+	password_salt VARCHAR(256),
+	password_hash VARCHAR(256)
 );
 
 CREATE TABLE session (
 	id SERIAL PRIMARY KEY,
-	player_id INTEGER REFERENCES player (id),
 	token VARCHAR(256),
-	expiry_time TIMESTAMP
+	expiry_time TIMESTAMP,
+	user_id INTEGER REFERENCES user (id)
+);
+
+CREATE TABLE game_stakes (
+	id SERIAL PRIMARY KEY,
+	ante BIGINT,
+	min_bet BIGINT,
+	max_bet BIGINT
+);
+
+CREATE TABLE game_status (
+	id SERIAL PRIMARY KEY,
+	description TEXT
 );
 
 CREATE TABLE game (
 	id SERIAL PRIMARY KEY,
-	name TEXT,
+	name VARCHAR(256),
 	description TEXT,
-	start_time TIMESTAMP,
-	finish_time TIMESTAMP
+	slug VARCHAR(64),
+	stakes INTEGER REFERENCES game_stakes (id),
+	status INTEGER REFERENCES game_status (id)
+);
+
+CREATE TABLE round_phase (
+	id SERIAL PRIMARY KEY,
+	description TEXT
 );
 
 CREATE TABLE game_round (
 	id SERIAL PRIMARY KEY,
+	pot BIGINT,
 	game_id INTEGER REFERENCES game (id),
-	stakes BIGINT,
-	pot BIGINT
+	phase INTEGER REFERENCES round_phase (id)
+);
+
+CREATE TABLE card (
+	id SERIAL PRIMARY KEY,
+	suit VARCHAR(8),
+	rank VARCHAR(8)
+);
+
+CREATE TABLE round_hand (
+	id SERIAL PRIMARY KEY,
+	card_1 REFERENCES card (id),
+	card_2 REFERENCES card (id),
+	card_3 REFERENCES card (id),
+	card_4 REFERENCES card (id),
+	card_5 REFERENCES card (id)
 );
 
 CREATE TABLE game_player (
 	id SERIAL PRIMARY KEY,
-	player_id INTEGER REFERENCES player (id),
 	position INTEGER,
-	total_won BIGINT
+	starting_cash BIGINT,
+	final_cash BIGINT,
+	hand INTEGER REFERENCES round_hand (id)
+	user_id INTEGER REFERENCES user (id)
 );
 
-CREATE TABLE round_action (
-	id SERIAL PRIMARY KEY,
-	round_seat_id INTEGER REFERENCES round_seat (id),
-	pass BOOLEAN
+CREATE TABLE player_stats (
+	user_id INTEGER REFERENCES user (id),
+	best_hand INTEGER REFERENCES round_hand (id),
+	total_hands INTEGER,
+	total_cash BIGINT
 );
