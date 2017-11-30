bef002a9b3cb946734562217956299cc357552b3
Clayton Jian <claytonj@sfu.ca>
2017-11-29 20:49:27 -0800

    Changed check, fold, and call buttons to work for play.html

--- static/js/game.js
+++ static/js/game.js
@@ -5,16 +5,12 @@ $(document).ready(function(){
 	function nextTurn(){
 		// if it's your turn, draw blue outline and animate it
 		if($('.player-1').find('.image').hasClass('yourTurn')){
-			$('.yourTurn').parent().addClass('folded');
-			$('.yourTurn').parent().parent().find('.hand').addClass('folded');
 			$('.player-1').find('.image').removeClass('yourTurn');
 			$('.player-1').find('.timer').removeClass('startTimer');
 			$('.player-2').find('.image').addClass('yourTurn');
 			$('.player-2').find('.timer').addClass('startTimer');
 		}
 		else if($('.player-2').find('.image').hasClass('yourTurn')){
-			$('.yourTurn').parent().addClass('folded');
-			$('.yourTurn').parent().parent().find('.hand').addClass('folded');
 			$('.player-2').find('.image').removeClass('yourTurn');
 			$('.player-2').find('.timer').removeClass('startTimer');
 			if(numberOfPlayers >= 3){
@@ -27,8 +23,6 @@ $(document).ready(function(){
 			}
 		}
 		else if($('.player-3').find('.image').hasClass('yourTurn')){
-			$('.yourTurn').parent().addClass('folded');
-			$('.yourTurn').parent().parent().find('.hand').addClass('folded');
 			$('.player-3').find('.image').removeClass('yourTurn');
 			$('.player-3').find('.timer').removeClass('startTimer');
 			if(numberOfPlayers >= 4){
@@ -41,8 +35,6 @@ $(document).ready(function(){
 			}
 		}
 		else if($('.player-4').find('.image').hasClass('yourTurn')){
-			$('.yourTurn').parent().addClass('folded');
-			$('.yourTurn').parent().parent().find('.hand').addClass('folded');
 			$('.player-4').find('.image').removeClass('yourTurn');
 			$('.player-4').find('.timer').removeClass('startTimer');
 			if(numberOfPlayers >= 5){
@@ -55,8 +47,6 @@ $(document).ready(function(){
 			}
 		}
 		else if($('.player-5').find('.image').hasClass('yourTurn')){
-			$('.yourTurn').parent().addClass('folded');
-			$('.yourTurn').parent().parent().find('.hand').addClass('folded');
 			$('.player-5').find('.image').removeClass('yourTurn');
 			$('.player-5').find('.timer').removeClass('startTimer');
 			if(numberOfPlayers == 6){
@@ -69,8 +59,6 @@ $(document).ready(function(){
 			}
 		}
 		else{
-			$('.yourTurn').parent().addClass('folded');
-			$('.yourTurn').parent().parent().find('.hand').addClass('folded');
 			$('.player-6').find('.image').removeClass('yourTurn');
 			$('.player-6').find('.timer').removeClass('startTimer');
 			$('.player-1').find('.image').addClass('yourTurn');
@@ -88,6 +76,7 @@ $(document).ready(function(){
 	$('#game-menu').on('click', '#fold', function(){
 		$('.startTimer').stop(true, true);
 		$('.yourTurn').parent().addClass('folded');
+		$('.yourTurn').parent().parent().find('.hand').addClass('folded');
 		nextTurn();
 	});
 	$('#game-menu').on('click', '#raiseButton', function(){
