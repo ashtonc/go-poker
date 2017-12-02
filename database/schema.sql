40b322b1336c70b295570b65685048cb1130470b
Matthew <cmt16@sfu.ca>
2017-12-01 22:26:59 -0800

    removed/replaced unwanted fields in table 'account'

--- database/schema.sql
+++ database/schema.sql
@@ -3,10 +3,11 @@ CREATE TABLE account (
 	username VARCHAR(32),
 	name VARCHAR(256),
 	email VARCHAR(128),
-	picture_slug VARCHAR(128),
-	description TEXT,
-	password_salt VARCHAR(256),
-	password_hash VARCHAR(256)
+	password VARCHAR(256)
+	-- picture_slug VARCHAR(128),
+	-- description TEXT,
+	-- password_salt VARCHAR(256),
+	-- password_hash VARCHAR(256)
 );
 
 CREATE TABLE session (
