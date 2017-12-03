ff5ba8f617853e9c11c6d83a7b529d039adc24da
Ashton <acharbon@sfu.ca>
2017-12-02 19:10:59 -0800

    Added basic websocket connection code. Not yet customized for our application or even close to working.

---
+++ connections/client.go
@@ -0,0 +1,67 @@
+package connections
+
+import (
+	"github.com/gorilla/websocket"
+	uuid "github.com/satori/go.uuid"
+)
+
+// This code 'borrowed' from https://outcrawl.com/realtime-collaborative-drawing-go/
+
+type Client struct {
+	id  string
+	hub *Hub
+
+	// Websocket connection
+	socket *websocket.Conn
+
+	// Buffered channel of outbound messages
+	outbound chan []byte
+}
+
+// Client constructor
+func newClient(hub *Hub, socket *websocket.Conn) *Client {
+	return &Client{
+		id:       uuid.NewV4().String(),
+		hub:      hub,
+		socket:   socket,
+		outbound: make(chan []byte),
+	}
+}
+
+// Read a message from the client and send it to the hub
+func (client *Client) read() {
+	defer func() {
+		client.hub.unregister <- client
+	}()
+	for {
+		_, data, err := client.socket.ReadMessage()
+		if err != nil {
+			break
+		}
+		client.hub.onMessage(data, client)
+	}
+}
+
+// Send messages to the client from the outbound channel
+func (client *Client) write() {
+	for {
+		select {
+		case data, ok := <-client.outbound:
+			if !ok {
+				client.socket.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			client.socket.WriteMessage(websocket.TextMessage, data)
+		}
+	}
+}
+
+func (client Client) run() {
+	go client.read()
+	go client.write()
+}
+
+func (client Client) close() {
+	client.socket.Close()
+	close(client.outbound)
+}
